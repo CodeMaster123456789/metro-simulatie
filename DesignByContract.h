419d981506327ec724bae3ca92bb64c6221070c0
David123457 <128138688+David123457@users.noreply.github.com>
2023-03-17 11:27:03 +0100

    verbeterd DesignByContract.h

--- DesignByContract.h
+++ DesignByContract.h
@@ -8,8 +8,20 @@
 
 #include <assert.h>
 
+#if defined(__MINGW32__)
+
 #define REQUIRE(assertion, what) \
-	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+    if (!(assertion)) _assert (what, __FILE__, __LINE__)
 
 #define ENSURE(assertion, what) \
-	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+    if (!(assertion)) _assert (what, __FILE__, __LINE__)
+
+#else
+
+#define REQUIRE(assertion, what) \
+    if (!(assertion)) __assert (what, __FILE__, __LINE__)
+
+#define ENSURE(assertion, what) \
+    if (!(assertion)) __assert (what, __FILE__, __LINE__)
+
+#endif
